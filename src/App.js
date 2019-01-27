480a74ff229f9c4147133468b54ef7624ebf3312
Binh Tran <thanhbinh.tran93@gmail.com>
2019-01-27 19:04:51 +0200

    Add FileInput component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import FileInput from "./components/FileInput";
 import OpeningHours from "./components/OpeningHours";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FileInput />
         <OpeningHours />
       </div>
     );
--- src/components/FileInput/index.js
+++ src/components/FileInput/index.js
@@ -1,7 +1,38 @@
 import React from "react";
 
 const FileInput = () => {
-  return <div>file input</div>;
+  const handleFileChange = event => {
+    const reader = new FileReader();
+    reader.onload = onReaderLoad;
+    reader.readAsText(event.target.files[0]);
+  };
+
+  const onReaderLoad = event => {
+    const openingHoursObj = JSON.parse(event.target.result);
+    console.log(openingHoursObj);
+
+    let arr = Object.keys(openingHoursObj).map(key => {
+      return {
+        day: key,
+        data: openingHoursObj[key]
+      };
+    });
+
+    console.log(arr);
+  };
+
+  return (
+    <div>
+      <label htmlFor="file">Choose a JSON file</label>
+      <input
+        type="file"
+        id="file"
+        name="file"
+        accept="application/json"
+        onChange={handleFileChange}
+      />
+    </div>
+  );
 };
 
 export default FileInput;
